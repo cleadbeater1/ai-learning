9d88d67e3a45113eef23cf3ef9becbd40e3fe599
C Leadbeater <cleadbeater@outlook.com>
2025-12-13 13:47:13 -0600

    Day 1: First Python Program

---
+++ src/day1.py
@@ -0,0 +1,15 @@
+number_of_models = 3
+model_name = "Price Predictor"
+
+print(number_of_models)
+print(model_name)
+
+total_data = 1000
+training_data = total_data * 0.8
+testing_data = total_data * 0.2
+
+print(training_data)
+print(testing_data)
+
+user_name = input("What is your name? ")
+print("Welcome, ", user_name)
