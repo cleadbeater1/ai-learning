af8c738cd3c2a1866ef8a9ded18164470009015a
C Leadbeater <cleadbeater@outlook.com>
2025-12-14 12:03:30 -0600

    Day 2: decision logic through repetition

---
+++ src/day2_decisions.py
@@ -0,0 +1,16 @@
+accuracy = float(input("Enter your accuracy: "))
+dataset = int(input("Enter the size of the dataset: "))
+
+if accuracy >= 0.85 or dataset >= 1000:
+    print("Accept Model")
+else:
+    print("Reject Model")
+
+# Today I leanred that:
+# - If statements on run when conditions are true
+# - indentation controls logic 
+# - computers are very literal
+
+
+
+
